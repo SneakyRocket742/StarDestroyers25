eb8b7ed59689f115e871a587e44455332287a02b
Merek Sievert <mereksievert@gmail.com>
2024-06-27 21:31:13 -0600

    first commit- currently do not have main code, but do have accel and servo file. Acceldoes not work, and servo only turns 90 degrees.

---
+++ BMP388_test.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_bmp3xx
+
+# I2C setup
+i2c = board.I2C()  # uses board.SCL and board.SDA
+bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+
+bmp.pressure_oversampling = 8
+bmp.temperature_oversampling = 2
+
+while True:
+    print(
+        "Pressure: {:6.4f}  Temperature: {:5.2f}".format(bmp.pressure, bmp.temperature)
+    )
+    time.sleep(1)
