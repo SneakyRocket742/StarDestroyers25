c3803738e988f0500f96c8e37f128721ba32ecbc
Merek Sievert <mereksievert@gmail.com>
2024-07-01 15:33:57 -0600

    Modified Servo_test.py by increasing pulse width of the servo, resulting in servo turning approximately 100 degrees.

--- Servo_test.py
+++ Servo_test.py
@@ -1,7 +1,11 @@
 from gpiozero import Servo
 from time import sleep
+Correc = 0.5
+maxPW = (2.0 + Correc) / 1000
+minPW = (1.0 - Correc) / 1000
 
-servo = Servo(12)
+servo = Servo(12,min_pulse_width=minPW,max_pulse_width=maxPW)
+
 
 while True:
 #	servo.min()
@@ -9,7 +13,7 @@ while True:
 #	servo.mid()
 #	sleep(0.5)
 #	servo.max()
-#	sleep(0.5) 
+#	sleep(0.5)
 	servo.min()
 	sleep(1)
 	servo.max()
