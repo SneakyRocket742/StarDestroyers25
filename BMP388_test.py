ef02898ec3f36c679a886c60ac5448bfe8b2ba54
Merek Sievert <mereksievert@gmail.com>
2024-08-18 01:20:04 -0600

    Changed board module to busio module and defined I2C addr and pins. This allowed alitimeter to work.

--- BMP388_test.py
+++ BMP388_test.py
@@ -1,13 +1,15 @@
+																								#Currently Untested!!
+
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
-import time 			# Imports the sleep module from time, used for delaying the code
-import board 			# Imports the board module
-import adafruit_bmp3xx 				# Imports the module responsible for controlling the sensor
+from time import sleep 			# Imports the sleep module from time, used for delaying the code
+import busio 			# Imports the busio module 					# type: ignore
+import adafruit_bmp3xx 				# Imports the module responsible for controlling the sensor 					 # type: ignore
 
 # I2C setup
-i2c = board.I2C() 			# Defining i2c addr, uses board.SCL and board.SDA
-bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c) 			# Making a sensor object name "BMP"
+i2c = busio.I2C(3, 2) 			# Defining i2c addr, uses busio.SCL and busio.SDA
+bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c, address = 0x76) 			# Making a sensor object name "BMP"
 
 bmp.pressure_oversampling = 8 			# Defines oversampling value for pressure
 bmp.temperature_oversampling = 2 			# Defines oversampling value for temperature
@@ -16,4 +18,4 @@ while True: 			# Runs code repeatedly
     print(				#Prints out the pressure and temperature
         "Pressure: {:6.4f}  Temperature: {:5.2f}".format(bmp.pressure, bmp.temperature)
     )
-    time.sleep(1) 			#Delays for 1 second
+    sleep(1) 			#Delays for 1 second
